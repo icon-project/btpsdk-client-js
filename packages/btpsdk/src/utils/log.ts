e15ebec8577c0a10c24ba91e5ec16cb1e83ba6e5
EunKi Yu <yueunki@parametacorp.com>
2023-11-17 15:24:31 +0900

    Unuse winston library for browser compatibility

--- packages/btpsdk/src/utils/log.ts
+++ packages/btpsdk/src/utils/log.ts
@@ -1,33 +1,41 @@
-import winston from 'winston';
+//import winston from 'winston';
 
 import type {
-  Logger,
+  //Logger,
   LoggerOptions
 } from 'winston';
 
 import {
-  merge
+  //merge
 } from './index';
 
-const { combine, timestamp, printf } = winston.format;
+//const { combine, timestamp, printf } = winston.format;
 
-const format = (label: string) => {
-  return combine(
-    winston.format.label({ label }),
-    timestamp({ format: 'HH:mm:ss' }),
-    printf(({ timestamp, level, label, message }) => `${level.toUpperCase()[0]}|${timestamp}|${label}| ${message}`)
-  );
-}
+// const format = (label: string) => {
+//   return combine(
+//     winston.format.label({ label }),
+//     timestamp({ format: 'HH:mm:ss' }),
+//     printf(({ timestamp, level, label, message }) => `${level.toUpperCase()[0]}|${timestamp}|${label}| ${message}`)
+//   );
+// }
 
-const LOG_LEVEL = process.env.BTP_LOG_LEVEL || 'error';
+// const LOG_LEVEL = process.env.BTP_LOG_LEVEL || 'error';
 
-const OPTIONS = {
-  level: LOG_LEVEL,
-  transports: [ new winston.transports.Console() ],
-  format: format('unknown'),
+// const OPTIONS = {
+//   level: LOG_LEVEL,
+//   transports: [ new winston.transports.Console() ],
+//   format: format('unknown'),
+// }
+
+interface Logger {
+  log: (...args: Array<any>) => void;
+  debug: (...args: Array<any>) => void
+  info: (...args: Array<any>) => void
+  warn: (...args: Array<any>) => void
 }
 
 export function getLogger(mod: string, options?: LoggerOptions): Logger {
-  const base = { ...OPTIONS, format: format(mod) };
-  return winston.createLogger(options != null ? merge(base, { ...options }) : base);
+  return console;
+  // const base = { ...OPTIONS, format: format(mod) };
+  // return winston.createLogger(options != null ? merge(base, { ...options }) : base);
 }
